217233401c2ea12351ee65e227f24d46eebd5775
Ipfi Lidzhade <g16L5376@ict.ru.ac.za>
2019-03-20 12:56:22 +0200

    added start function

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -1,14 +1,39 @@
 ﻿module Oware
 
+open System.Linq.Expressions
+
 type StartingPosition =
     | South
     | North
 
+type GameState =
+| STurn 
+| NTurn
+| NWon
+| SWon
+| Draw
+| Initial
+
+type Player = {
+  Score: int
+  Houses: int*int*int*int*int*int
+}
+type Board = {
+  NorthPlayer: Player 
+  SouthPlayer: Player
+  GameState: GameState
+ }
+ 
 let getSeeds n board = failwith "Not implemented"
 
 let useHouse n board = failwith "Not implemented"
 
-let start position = failwith "Not implemented"
+let start position = 
+    let board =
+        { NorthPlayer = {Score = 0; Houses = 4,4,4,4,4,4}
+          SouthPlayer = {Score = 0; Houses = 4,4,4,4,4,4}
+          GameState = STurn}
+    board
 
 let score board = failwith "Not implemented"
 
