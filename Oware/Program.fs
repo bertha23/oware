85de4a3f8aade2c2d5cde9eb47d5cbdcd93caafc
Ipfi Lidzhade <g16L5376@ict.ru.ac.za>
2019-03-21 13:10:39 +0200

    get seeds function added

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -22,17 +22,61 @@ type Board = {
   NorthPlayer: Player 
   SouthPlayer: Player
   GameState: GameState
+  StartingPosition: StartingPosition
  }
  
-let getSeeds n board = failwith "Not implemented"
+let getSeeds n board = 
+    match n with
+    | 1 ->
+        let a,_,_,_,_,_ = board.SouthPlayer.Houses
+        a
+    | 2 ->
+        let _,a,_,_,_,_ = board.SouthPlayer.Houses
+        a
+    | 3 ->
+        let _,_,a,_,_,_ = board.SouthPlayer.Houses
+        a
+    | 4 ->
+        let _,_,_,a,_,_ = board.SouthPlayer.Houses
+        a
+    | 5 ->
+        let _,_,_,_,a,_ = board.SouthPlayer.Houses
+        a
+    | 6 ->
+        let _,_,_,_,_,a = board.SouthPlayer.Houses
+        a
+    | 7 ->
+        let a,_,_,_,_,_ = board.NorthPlayer.Houses
+        a
+    | 8 ->
+        let _,a,_,_,_,_ = board.NorthPlayer.Houses
+        a
+    | 9 ->
+        let _,_,a,_,_,_ = board.NorthPlayer.Houses
+        a
+    | 10 ->
+        let _,_,_,a,_,_ = board.NorthPlayer.Houses
+        a
+    | 11 ->
+        let _,_,_,_,a,_ = board.NorthPlayer.Houses
+        a
+    | 12 ->
+        let _,_,_,_,_,a = board.NorthPlayer.Houses
+        a
+    | 0 -> failwith "Invalid house number"
 
 let useHouse n board = failwith "Not implemented"
 
 let start position = 
+    let a =
+        match position with
+        | South -> STurn
+        | North -> NTurn
     let board =
         { NorthPlayer = {Score = 0; Houses = 4,4,4,4,4,4}
           SouthPlayer = {Score = 0; Houses = 4,4,4,4,4,4}
-          GameState = STurn}
+          GameState = a
+          StartingPosition = position}
     board
 
 let score board = failwith "Not implemented"
