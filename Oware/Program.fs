51f9144ba33115233df3c7945b4819fb60cd3267
lesedipitsi123 <lesedipitsi@gmail.com>
2019-03-28 14:42:42 +0200

    Working on getSeeds

--- Oware/Program.fs
+++ Oware/Program.fs
@@ -1,18 +1,60 @@
 ﻿module Oware
 
+type GameState= 
+    | STurn
+    | NTurn
+    | Nwon
+    | SWon
+    | Draw 
+    | Initial
+
+// House Record
+type House = {
+    Seeds: int
+    Index: int
+}
+
+// Player Record 
+type Player = {
+    Score: int
+    Houses: House*House*House*House*House*House
+}
+
+
+// Board data structure here ======
+type Board = {
+    HighScore : int
+    NorthPlayer : Player
+    SouthPlayer : Player 
+    GameState : GameState
+}
+
 type StartingPosition =
     | South
     | North
 
-let getSeeds n board = failwith "Not implemented"
+let getSeeds n board = 
+     failwith "Not implemented"
+    // determine when borad state first
+   
 
-let useHouse n board = failwith "Not implemented"
+    // Iterate n times until you reach hosue n
+let useHouse n board =
+    failwith "Not implemented"
 
-let start position = failwith "Not implemented"
+let start position = 
+    
+    match position with
+    | South -> "South's turn"
+    | North -> "North's turn" 
 
 let score board = failwith "Not implemented"
 
-let gameState board = failwith "Not implemented"
+let gameState board = 
+    
+    
+    
+    failwith "Not implemented"
 
 [<EntryPoint>]
 let main _ =
